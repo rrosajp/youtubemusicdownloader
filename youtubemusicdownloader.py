d17e367e4e902b48a12f920bb824fe16fbb9b591
R. M <50295204+glomatico@users.noreply.github.com>
2022-01-22 03:13:30 -0300

    Update youtubemusicdownloader.py

--- youtubemusicdownloader.py
+++ youtubemusicdownloader.py
@@ -17,16 +17,18 @@ albumyear = albuminfo['year']
 albumtotaltracks = albuminfo['trackCount']
 albumduration = albuminfo['duration']
 albumtrackinfo = albuminfo['tracks']
+albumcover = albuminfo['thumbnails'][0]['url'][:-15] + '=w1024-h1024-l90-rj'
 print("\nAlbum info:")
 print("\nName: " + albumname)
 print("Artist: " + albumartistname)
 print("Year: " + albumyear)
 print("Total track(s): " + str(albumtotaltracks))
-print("Duration: " + albumduration + "\n")
+print("Duration: " + albumduration)
+print("Cover: " + albumcover + "\n")
 arrayTratado = []
-print("Album tracklist:\n")
+print("Album tracklist (Track number/Artist/Title/Explicit):\n")
 for i in range(albumtotaltracks):
-    albumtrackartist = 'churrasqueira controle remoto'
+    albumtrackartist = ''
     if albumtrackinfo[0]['artists'] == None:
         albumtrackartist = albumartistname
     else:
@@ -36,8 +38,8 @@ for i in range(albumtotaltracks):
     json_dump = json.dumps(dataSetTrack)
     json_object = json.loads(json_dump)
     arrayTratado.append(json_object)
-    print(str(i + 1) + " - " + json_object['artists'] + " - " + json_object['title'])
-print('\nDownloading album...')
+    print(str(i + 1) + " - " + json_object['artists'] + " - " + json_object['title'] + " - " + str(json_object['explicit']))
+print('\nDownloading album...\n')
 ydl_opts = {
     'format': '141/140',
     'cookiefile': 'cookies.txt',
@@ -45,3 +47,4 @@ ydl_opts = {
 }
 with yt_dlp.YoutubeDL(ydl_opts) as ydl:
     ydl.download(['https://music.youtube.com/playlist?list=' + playlistidinput])
+print('\nAdding tags...\n')
