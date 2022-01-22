6c3e1f3f95cd13c6d4557894445684854f7c45e7
R. M <50295204+glomatico@users.noreply.github.com>
2022-01-22 00:16:45 -0300

    Update youtubemusicdownloader.py

--- youtubemusicdownloader.py
+++ youtubemusicdownloader.py
@@ -1,6 +1,7 @@
 from queue import Empty
 from types import NoneType
 from ytmusicapi import YTMusic
+import json
 
 ytmusic = YTMusic('headers_auth.json')
 playlistidinput = input('Insert a YouTube Music album playlist ID: ')
@@ -9,7 +10,30 @@ while albumid is None:
     playlistidinput = input('Invalid playlist ID. Insert a YouTube Music album playlist ID: ')
     albumid = ytmusic.get_album_browse_id(playlistidinput)
 albuminfo = ytmusic.get_album(albumid)
-print("Album name: " + albuminfo['title'])
-print("Album artist name: " + albuminfo['artists'][0]['name'])
-print("Album year: " + albuminfo['year'])
-print("Album total track(s): " + str(albuminfo['trackCount']))
+albumname = albuminfo['title']
+albumartistname = albuminfo['artists'][0]['name']
+albumyear = albuminfo['year']
+albumtotaltracks = albuminfo['trackCount']
+albumduration = albuminfo['duration']
+albumtrackinfo = albuminfo['tracks']
+print("\nAlbum info:")
+print("\nName: " + albumname)
+print("Artist: " + albumartistname)
+print("Year: " + albumyear)
+print("Total track(s): " + str(albumtotaltracks))
+print("Duration: " + albumduration + "\n")
+arrayTratado = []
+print("Album tracklist:\n")
+for i in range(albumtotaltracks):
+    albumtrackartist = 'churrasqueira controle remoto'
+    if albumtrackinfo[0]['artists'] == None:
+        albumtrackartist = albumartistname
+    else:
+        albumtrackartist = albumtrackinfo[i]['artists'][0]['name']
+    dataSetTrack = { 'title': albumtrackinfo[i]['title'],  'artists': albumtrackartist
+        ,'explicit': albumtrackinfo[i]['isExplicit']}
+    json_dump = json.dumps(dataSetTrack)
+    json_object = json.loads(json_dump)
+    arrayTratado.append(json_object)
+    print(str(i + 1) + " - " + json_object['artists'] + " - " + json_object['title'])
+print(arrayTratado[8]['artists'])
