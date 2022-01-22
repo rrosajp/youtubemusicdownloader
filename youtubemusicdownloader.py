58d48cde6ce74f441f297f6cca1cfccb0900390c
R. M <50295204+glomatico@users.noreply.github.com>
2022-01-22 20:05:06 -0300

    Update youtubemusicdownloader.py

--- youtubemusicdownloader.py
+++ youtubemusicdownloader.py
@@ -3,7 +3,9 @@ from types import NoneType
 from ytmusicapi import YTMusic
 import json
 import yt_dlp
-from mutagen.mp4 import MP4
+from mutagen.mp4 import MP4, MP4Cover
+import urllib.request
+import os
 
 ytmusic = YTMusic('headers_auth.json')
 playlistidinput = input('Insert a YouTube Music album playlist ID: ')
@@ -18,7 +20,7 @@ albumyear = albuminfo['year']
 albumtotaltracks = albuminfo['trackCount']
 albumduration = albuminfo['duration']
 albumtrackinfo = albuminfo['tracks']
-albumcover = albuminfo['thumbnails'][0]['url'][:-15] + '=w1024-h1024-l90-rj'
+albumcover = albuminfo['thumbnails'][3]['url']
 print("\nAlbum info:")
 print("\nName: " + albumname)
 print("Artist: " + albumartist)
@@ -34,27 +36,33 @@ for i in range(albumtotaltracks):
         albumtrackartist = albumartist
     else:
         albumtrackartist = albumtrackinfo[i]['artists'][0]['name']
-    dataSetTrack = { 'title': albumtrackinfo[i]['title'],  'artists': albumtrackartist
-        ,'explicit': albumtrackinfo[i]['isExplicit']}
+    dataSetTrack = { 'title': albumtrackinfo[i]['title'],  'artists': albumtrackartist}
     json_dump = json.dumps(dataSetTrack)
     json_object = json.loads(json_dump)
     arrayTratado.append(json_object)
-    print(str(i + 1) + " - " + json_object['artists'] + " - " + json_object['title'] + " - " + str(json_object['explicit']))
+    print(str(i + 1) + " - " + json_object['artists'] + " - " + json_object['title'])
 print('\nDownloading album...\n')
 ydl_opts = {
     'format': '141/140',
     'cookiefile': 'cookies.txt',
-    'outtmpl': '%(playlist_index)s.m4a'
+    for b range(albumtotaltracks):
+        'outtmpl': '%(playlist_index)02d' + " " + arrayTratado[b]['title'] + '.m4a'
 }
 with yt_dlp.YoutubeDL(ydl_opts) as ydl:
     ydl.download(['https://music.youtube.com/playlist?list=' + playlistidinput])
-print('\nAdding tags...\n')
+print('\nAdding tags & renaming files...\n')
+urllib.request.urlretrieve(albumcover, 'Cover.jpg')
 for a in range(albumtotaltracks):
-    tags = MP4(str(a + 1) + '.m4a').tags
+    tags = MP4(str("%02d" % (a + 1,)) + '.m4a').tags
     tags['\xa9nam'] = arrayTratado[a]['title']
     tags['\xa9alb'] = albumname
     tags['aART'] = albumartist
     tags['\xa9day'] = albumyear
     tags['\xa9ART'] = arrayTratado[a]['artists']
-    tags['trkn'] = str(albumtotaltracks)
-    tags.save(str(a + 1) + '.m4a')
+    with open('Cover.jpg', "rb") as cover:
+        tags["covr"] = [
+        MP4Cover(cover.read(), imageformat=MP4Cover.FORMAT_JPEG)
+        ]
+    tags.save(str("%02d" % (a + 1,)) + '.m4a')
+    os.rename(str("%02d" % (a + 1,)) + '.m4a', str("%02d" % (a + 1,)) + " " + arrayTratado[a]['title'] +'.m4a')
+print('\nDone!\n')
