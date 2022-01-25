ffad3e6d551de16e1313e68ca5acb71f24153f96
R. M <50295204+glomatico@users.noreply.github.com>
2022-01-24 23:17:20 -0300

    initial release

--- ytmusicalbumdownloader.py
+++ ytmusicalbumdownloader.py
@@ -1,5 +1,8 @@
+from logging import exception
 from ytmusicapi import YTMusic
 import yt_dlp
+from mutagen.mp4 import MP4, MP4Cover
+import urllib.request
 
 # ask user input
 ytmusic = YTMusic()
@@ -15,28 +18,30 @@ while albumBrowseId is None:
 albumRawInfo = ytmusic.get_album(albumBrowseId)
 
 albumTrackArtist = []
-albumTrackList = []
+albumTrackName = []
 albumName = []
 albumArtist = []
+albumTrackPosition = []
+albumTrackVideoId = []
+albumCoverUrl = []
 
-albumTrackListFixed = []
+albumTrackNameFixed = []
 albumNameFixed = []
 albumArtistFixed = []
-albumTrackCount = []
+
 
 for i in range(albumRawInfo['trackCount']):
 
     # save track artist
     if albumRawInfo['tracks'][i]['artists'] == None:
         albumTrackArtist.append(albumRawInfo['artists'][0]['name'])
-        albumTrackArtist.append(albumRawInfo['artists'][0]['name'])
     else:
         albumTrackArtist.append(albumRawInfo['tracks'][i]['artists'][0]['name'])
 
     # save album track list
-    albumTrackList.append(albumRawInfo['tracks'][i]['title'])
+    albumTrackName.append(albumRawInfo['tracks'][i]['title'])
 
-    albumTrackListFixed.append(albumRawInfo['tracks'][i]['title'])
+    albumTrackNameFixed.append(albumRawInfo['tracks'][i]['title'])
 
 # save album name
 albumName.append(albumRawInfo['title'])
@@ -48,24 +53,24 @@ albumArtist.append(albumRawInfo['artists'][0]['name'])
 
 albumArtistFixed.append(albumRawInfo['artists'][0]['name'])
 
-# save album track count and format it
+# save album track count & format it
 for i in range(albumRawInfo['trackCount']):
-    if albumRawInfo['trackCount'] > 100:
-        albumTrackCount.append("%03d" % (i + 1))
-    else:
-        albumTrackCount.append("%02d" % (i + 1))
+    albumTrackPosition.append("%02d" % (i + 1))
+
+# save album cover url
+albumCoverUrl = albumRawInfo['thumbnails'][0]['url'].replace('w60-h60', 'w1200-h1200')
 
 # remove illegal characters from album track list
-for i in range(len(albumTrackList)):
-    albumTrackListFixed[i] = albumTrackListFixed[i].replace('\\', '_')
-    albumTrackListFixed[i] = albumTrackListFixed[i].replace('/', '_')
-    albumTrackListFixed[i] = albumTrackListFixed[i].replace(':', '_')
-    albumTrackListFixed[i] = albumTrackListFixed[i].replace('*', '_')
-    albumTrackListFixed[i] = albumTrackListFixed[i].replace('?', '_')
-    albumTrackListFixed[i] = albumTrackListFixed[i].replace('"', '_')
-    albumTrackListFixed[i] = albumTrackListFixed[i].replace('<', '_')
-    albumTrackListFixed[i] = albumTrackListFixed[i].replace('>', '_')
-    albumTrackListFixed[i] = albumTrackListFixed[i].replace('|', '_')
+for i in range(len(albumTrackName)):
+    albumTrackNameFixed[i] = albumTrackNameFixed[i].replace('\\', '_')
+    albumTrackNameFixed[i] = albumTrackNameFixed[i].replace('/', '_')
+    albumTrackNameFixed[i] = albumTrackNameFixed[i].replace(':', '_')
+    albumTrackNameFixed[i] = albumTrackNameFixed[i].replace('*', '_')
+    albumTrackNameFixed[i] = albumTrackNameFixed[i].replace('?', '_')
+    albumTrackNameFixed[i] = albumTrackNameFixed[i].replace('"', '_')
+    albumTrackNameFixed[i] = albumTrackNameFixed[i].replace('<', '_')
+    albumTrackNameFixed[i] = albumTrackNameFixed[i].replace('>', '_')
+    albumTrackNameFixed[i] = albumTrackNameFixed[i].replace('|', '_')
 
 # remove illegal characters from album artist
 albumArtistFixed[0] = albumArtistFixed[0].replace('\\', '_')
@@ -88,16 +93,36 @@ albumNameFixed[0] = albumNameFixed[0].replace('"', '_')
 albumNameFixed[0] = albumNameFixed[0].replace('<', '_')
 albumNameFixed[0] = albumNameFixed[0].replace('>', '_')
 albumNameFixed[0] = albumNameFixed[0].replace('|', '_')
-
+ 
 # show download step
 print('Downloading ' + albumName[0] + ' by ' + albumArtist[0] + '...')
 
+# download cover
+urllib.request.urlretrieve(albumCoverUrl, 'Cover.jpg')
+
 # start downloading
-for i in range(albumRawInfo['trackCount']):
-    ydl_opts = {
-    'format': '141/140',
-    'cookiefile': 'cookies.txt',
-    'outtmpl': str(albumTrackCount[i]) + ' ' + albumTrackListFixed[i] + '.m4a'
-}
-    with yt_dlp.YoutubeDL(ydl_opts) as ydl:
-        ydl.download('https://music.youtube.com/watch?v=' + albumRawInfo['tracks'][i]['videoId'])
+for i in range(len(albumTrackPosition)):
+    try:
+        ydl_opts = {
+        'format': '141/140',
+        'cookiefile': 'cookies.txt',
+        'outtmpl': 'YouTube Music/' + albumArtistFixed[0] + '/' + albumNameFixed[0] + '/' + str(albumTrackPosition[i]) + ' ' + albumTrackNameFixed[i] + '.m4a',
+        'playliststart': i + 1,
+        'playlistend': i + 1
+        }
+        with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+            ydl.download('https://music.youtube.com/playlist?list=' + playlistLinkInput)
+        tags = MP4('YouTube Music/' + albumArtistFixed[0] + '/' + albumNameFixed[0] + '/' + str(albumTrackPosition[i]) + ' ' + albumTrackNameFixed[i] + '.m4a').tags
+        tags['\xa9nam'] = albumTrackName[i]
+        tags['\xa9alb'] = albumName[0]
+        tags['aART'] = albumArtist[0]
+        tags['\xa9day'] = albumRawInfo['year']
+        tags['\xa9ART'] = albumTrackArtist[i]
+        tags['trkn'] = [(i + 1, albumRawInfo['trackCount'])]
+        with open('Cover.jpg', "rb") as cover:
+            tags["covr"] = [
+            MP4Cover(cover.read(), imageformat=MP4Cover.FORMAT_JPEG)
+        ]
+        tags.save('YouTube Music/' + albumArtistFixed[0] + '/' + albumNameFixed[0] + '/' + str(albumTrackPosition[i]) + ' ' + albumTrackNameFixed[i] + '.m4a')
+    except:
+        pass
